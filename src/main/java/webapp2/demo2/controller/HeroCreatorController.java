fe6873c2d8334634f07e7a82cc995e5b7ad6c02a
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2023-11-27 16:41:01 +0100

    getRandomClass() & getRandomLife added

--- src/main/java/webapp2/demo2/controller/HeroCreatorController.java
+++ src/main/java/webapp2/demo2/controller/HeroCreatorController.java
@@ -1,5 +1,10 @@
 package webapp2.demo2.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +14,7 @@ import webapp2.demo2.model.Hero;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 
@@ -20,6 +26,15 @@ public class HeroCreatorController {
         restTemplate = new RestTemplate();
     }
 
+    @Operation(summary = "Récupère les héros depuis l'API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Liste récupérée",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Hero.class))}),
+            @ApiResponse(responseCode = "400", description = "Liste non récupérée",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Liste non récupérée car elle n'existe pas",
+                    content = @Content)})
     @GetMapping(value = "/heroes", produces = "application/json")
     public List<LinkedHashMap> getHeroes() {
         String url = "http://localhost:8080/heroes";
@@ -27,17 +42,36 @@ public class HeroCreatorController {
         return response.getBody();
     }
 
+    @Operation(summary = "Récupère un nom au hassard depuis l'API random-word-api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mot random récupéré",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Hero.class))}),
+            @ApiResponse(responseCode = "400", description = "Mot non récupéré",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Mot non récupéré",
+                    content = @Content)})
     @GetMapping(value = "/random-name", produces = "application/json")
     public String getRandomName() {
-        String url = "https://random-word-api.herokuapp.com/word";
-        String response = restTemplate.getForObject(url, String.class);
-        return response.toString().replace('[', ' ').replace(']', ' ').trim();
+        ResponseEntity<Object> response = restTemplate.getForEntity("https://random-word-api.herokuapp.com/word", Object.class);
+        String randomName = response.getBody().toString().replace('[', ' ').replace(']', ' ').trim(); // permet de formater le String en enlevant les signes en trop
+        return randomName.substring(0, 1).toUpperCase() + randomName.substring(1); // permet de mettre la première lettre en capital
     }
 
-    @GetMapping(value = "/random-hero", produces = "application/json")
-    public LinkedHashMap getRandomHero() {
-        int rid = 0 + (int) (Math.random() * ((3 - 0) + 0));
-        return getHeroes().get(rid);
+    @Operation(summary = "Retourne un héros au hasard")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Héros au hasard retourné",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Hero.class))}),
+            @ApiResponse(responseCode = "400", description = "Héros non récupéré",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Héros non récupéré car il n'y en a pas",
+                    content = @Content)})
+    @GetMapping(value = "/random-hero")
+    public Object getRandomHero() {
+        List heroesList = getHeroes();
+        Collections.shuffle(heroesList);
+        return heroesList.get(0);
     }
 
     public int getMaxID() {
@@ -49,9 +83,31 @@ public class HeroCreatorController {
         return key;
     }
 
+    public String getRandomClass() {
+        ArrayList<String> classes = new ArrayList<>();
+        classes.add("Warrior");
+        classes.add("Wizard");
+        Collections.shuffle(classes);
+        return classes.get(0);
+    }
+
+    public int getRandomLife() {
+        int rLife = 0 + (int) (Math.random() * (10 - 0) + 0);
+        return rLife;
+    }
+
+    @Operation(summary = "Crée un personnage et l'ajoute dans l'app via l'API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Héros créé et ajouté",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Hero.class))}),
+            @ApiResponse(responseCode = "400", description = "Héros non créé et non ajouté",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Héros non créé et non ajouté",
+                    content = @Content)})
     @PostMapping("/random")
     public Object createHero() {
-        Hero newHero = new Hero(getMaxID() + 1, getRandomName(), "wizard", 10);
+        Hero newHero = new Hero(getMaxID() + 1, getRandomName(), getRandomClass(), getRandomLife());
         URI response = restTemplate.postForLocation("http://localhost:8080/heroes", newHero);
         return getHeroes();
     }
